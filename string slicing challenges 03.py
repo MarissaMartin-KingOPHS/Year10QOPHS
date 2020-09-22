14c630419a6133dc5fd50af0e3974a33fb7d0340
Marissa Martin-King <71695395+MarissaMartin-KingOPHS@users.noreply.github.com>
2020-09-22 10:10:20 +0100

    Add files via upload

---
+++ string slicing challenges 03.py
@@ -0,0 +1,5 @@
+# String slicing challenges 03
+
+name = (input("please enter your name:")).lower()
+surname =(input("please enter your surname:")).lower()
+print(name,"",surname)
---
+++ string slicing challenges 04.py
@@ -0,0 +1,5 @@
+# STRING SLICING CHALLENGES 04
+
+rhyme = str(input("enter the first line of a nursery rhyme>>>"))
+length = len(rhyme)
+print("this has",length,"letters")
